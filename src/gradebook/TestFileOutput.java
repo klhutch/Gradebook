dda85241b626bbc2dd1af302e858f0145db29b9c
Kate Hutchinson <klhutch@ccs.neu.edu>
2014-04-10 23:17:46 -0400

    Added another test

--- src/gradebook/TestFileOutput.java
+++ src/gradebook/TestFileOutput.java
@@ -3,7 +3,10 @@ package gradebook;
 public class TestFileOutput {
     public static void main(String[] args) {
         MyGradeBook gb = MyGradeBook.initializeWithFile("gradebook.txt");
-        System.out.println(gb.fileOutputCurrentGrades("testOutputCurGrades1.txt"));
+        gb.fileOutputCurrentGrades("testOutputCurGrades1.txt");
+        
+        MyGradeBook init = MyGradeBook.initializeWithFile("initial.txt");
+        init.fileOutputCurrentGrades("testOutputCurGrades2.txt");
         
     }
 }
