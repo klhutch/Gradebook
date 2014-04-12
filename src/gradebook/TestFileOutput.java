0e0058e850380f8a1cd8c77466840e53b646ae26
Kate Hutchinson <klhutch@ccs.neu.edu>
2014-04-11 23:35:41 -0400

    fixing stuff

--- src/gradebook/TestFileOutput.java
+++ src/gradebook/TestFileOutput.java
@@ -1,6 +1,20 @@
 package gradebook;
 
+/**Class TestFileOutput
+* Team 10
+* 
+* @author Kate Hutchinson (klhutch)
+* @author Jesse Oberstein (joberste)
+* @author Nathan Goodman (nmg49)
+* 
+* @version April 11th, 2014
+*
+*/
 public class TestFileOutput {
+    /**
+     * main method - runs the program
+     * @param args arguments to be used in main NOT USED
+     */
     public static void main(String[] args) {
         MyGradeBook gb = MyGradeBook.initializeWithFile("gradebook.txt");
         gb.fileOutputCurrentGrades("testOutputCurGrades1.txt");
