268adad67ce7bd8477c8e3d06d6050b98b299167
Kate Hutchinson <klhutch@ccs.neu.edu>
2014-04-11 23:44:37 -0400

    webcat comparator

--- src/gradebook/MyStudentComparator.java
+++ src/gradebook/MyStudentComparator.java
@@ -1,9 +1,20 @@
 package gradebook;
 
 import java.util.Comparator;
-
+/**Class MyStudentComparator
+ * 
+ * @author Kate Hutchinson (klhutch)
+ * @author Jesse Oberstein (joberste)
+ * @author Nathan Goodman (nmg49)
+ * 
+ * @version April 8th, 2014
+ *
+ */
 public class MyStudentComparator implements Comparator<Student> {
-
+    
+    /**
+     * return result from comparing student's usernames
+     */
     @Override
     public int compare(Student o1, Student o2) {
         return o1.getStudentUsername().compareTo(o2.getStudentUsername());
